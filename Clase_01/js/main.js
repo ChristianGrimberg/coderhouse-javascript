4731f25c15dee99fd515a2ed2ddfbaa5261abe7c
Christian Grimberg <christian.grimberg@outlook.com>
2026-03-12 21:17:51 -0300

    Update console.log comments to include data types

--- Clase_01/js/main.js
+++ Clase_01/js/main.js
@@ -4,7 +4,9 @@
 Comentarios multilinea
 */
 
-// Consola: vamos a ver el uso de console.log()
+// Consola: vamos a ver el uso de console.log() y tipos de datos
 
 console.log(43); // number: datos de tipo numérico
 console.log("Esto es un string"); // string: datos de tipo cadena de texto
+console.log(true); // boolean: datos de tipo lógico (verdadero o falso)
+console.log(false); // boolean: datos de tipo lógico (verdadero o falso)
