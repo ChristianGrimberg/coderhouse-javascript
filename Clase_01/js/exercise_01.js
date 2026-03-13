c4a92823a39a3cc5b7b689a9308c2255b5a76d39
Christian Grimberg <christian.grimberg@outlook.com>
2026-03-13 08:05:27 -0300

    Update exercise_01.js to simplify user data collection and enhance welcome message

--- Clase_01/js/exercise_01.js
+++ Clase_01/js/exercise_01.js
@@ -1,5 +1,5 @@
 /*
-Ejercicio 1: Voy a solicitarle al usuario sus datos: nombre, apellido, edad y foto de ambos lados de la tarjeta de credito.
+Ejercicio 1: Voy a solicitarle al usuario sus datos: nombre, apellido y edad.
 
  Después de solicitarle los datos, lo que quiero es mostrar un mensaje de bienvenida mencionando estos datos.
 
@@ -8,3 +8,11 @@ Quiero lograr algo llamado case sensitive: No distinguir mayusculas de minuscula
 .toUpperCase() -> convierte todo a mayusculas
 .toLowerCase() -> convierte todo a minusculas
 */
+
+let nombre = prompt("Ingrese su nombre:").toUpperCase();
+let apellido = prompt("Ingrese su apellido:").toUpperCase();
+let edad = prompt("Ingrese su edad:");
+
+let mensaje = `Bienvenido ${nombre} ${apellido}, tu edad es ${edad}`;
+
+alert(mensaje);
