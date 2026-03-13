c352a6784ee0a8e79bad554492b9145018c4d420
Christian Grimberg <christian.grimberg@outlook.com>
2026-03-12 21:24:27 -0300

    Enhance main.js with additional variable examples and console.log statements for better data type demonstration

--- Clase_01/js/main.js
+++ Clase_01/js/main.js
@@ -10,3 +10,14 @@ console.log(43); // number: datos de tipo numérico
 console.log("Esto es un string"); // string: datos de tipo cadena de texto
 console.log(true); // boolean: datos de tipo lógico (verdadero o falso)
 console.log(false); // boolean: datos de tipo lógico (verdadero o falso)
+
+// Variables: vamos a ver el uso de variables para almacenar datos
+// let o const (existe también var pero no se recomienda su uso por problemas de alcance)
+
+let nombre = "Juan"; // variable de tipo string
+const edad = 30; // variable de tipo number (constante, no se puede cambiar su valor)
+let esEstudiante = true; // variable de tipo boolean
+
+console.log(nombre); // Imprime el valor de la variable nombre
+console.log(edad); // Imprime el valor de la variable edad
+console.log(esEstudiante); // Imprime el valor de la variable esEstudiante
