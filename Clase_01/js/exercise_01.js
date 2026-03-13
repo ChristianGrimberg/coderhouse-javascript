84f9a75ff49e820a1e0bb3e4891514304afc8e46
Christian Grimberg <christian.grimberg@outlook.com>
2026-03-12 22:37:20 -0300

    Add exercise_01.js with user data collection exercise description

---
+++ Clase_01/js/exercise_01.js
@@ -0,0 +1,10 @@
+/*
+Ejercicio 1: Voy a solicitarle al usuario sus datos: nombre, apellido, edad y foto de ambos lados de la tarjeta de credito.
+
+ Después de solicitarle los datos, lo que quiero es mostrar un mensaje de bienvenida mencionando estos datos.
+
+Quiero lograr algo llamado case sensitive: No distinguir mayusculas de minusculas.
+
+.toUpperCase() -> convierte todo a mayusculas
+.toLowerCase() -> convierte todo a minusculas
+*/
